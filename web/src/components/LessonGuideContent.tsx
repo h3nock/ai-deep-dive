636ebee84d33ce4ae57613789e1cd6bc69e2b7df
henok3878 <81500872+henok3878@users.noreply.github.com>
2026-02-12 21:22:17 -0500

    allow trusted mdx expressions for next-mdx-remote v6

--- web/src/components/LessonGuideContent.tsx
+++ web/src/components/LessonGuideContent.tsx
@@ -16,100 +16,102 @@ import { ProjectRoadmap } from "@/components/mdx/ProjectRoadmap";
 import { SplitLayout } from "@/components/mdx/SplitLayout";
 import { Step } from "@/components/mdx/Step";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
 } from "@/components/mdx/Table";
 import { ThinkingProcess } from "@/components/mdx/ThinkingProcess";
 
 const FrequencyWaves = dynamic(() =>
-  import("@/components/mdx/FrequencyWaves").then((mod) => mod.FrequencyWaves)
+    import("@/components/mdx/FrequencyWaves").then((mod) => mod.FrequencyWaves)
 );
 const BinaryVsSmooth = dynamic(() =>
-  import("@/components/mdx/BinaryVsSmooth").then((mod) => mod.BinaryVsSmooth)
+    import("@/components/mdx/BinaryVsSmooth").then((mod) => mod.BinaryVsSmooth)
 );
 const RotationVisualization = dynamic(() =>
-  import("@/components/mdx/RotationVisualization").then(
-    (mod) => mod.RotationVisualization
-  )
+    import("@/components/mdx/RotationVisualization").then(
+        (mod) => mod.RotationVisualization
+    )
 );
 const EmbeddingSpace = dynamic(() =>
-  import("@/components/mdx/EmbeddingSpace").then((mod) => mod.EmbeddingSpace)
+    import("@/components/mdx/EmbeddingSpace").then((mod) => mod.EmbeddingSpace)
 );
 const LiveByteInspector = dynamic(() =>
-  import("@/components/mdx/LiveByteInspector").then(
-    (mod) => mod.LiveByteInspector
-  )
+    import("@/components/mdx/LiveByteInspector").then(
+        (mod) => mod.LiveByteInspector
+    )
 );
 const EncodingCompare = dynamic(() =>
-  import("@/components/mdx/EncodingCompare").then((mod) => mod.EncodingCompare)
+    import("@/components/mdx/EncodingCompare").then((mod) => mod.EncodingCompare)
 );
 const NormalizedStepSize = dynamic(() =>
-  import("@/components/mdx/NormalizedStepSize").then(
-    (mod) => mod.NormalizedStepSize
-  )
+    import("@/components/mdx/NormalizedStepSize").then(
+        (mod) => mod.NormalizedStepSize
+    )
 );
 
 const components = {
-  SplitLayout,
-  Step,
-  Description,
-  Action,
-  Command,
-  ProjectRoadmap,
-  ThinkingProcess,
-  EmbeddingSpace,
-  Callout,
-  ByteStream,
-  table: Table,
-  thead: TableHead,
-  tbody: TableBody,
-  tr: TableRow,
-  th: TableHeader,
-  td: TableCell,
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableHeader,
-  TableCell,
-  ComingSoon,
-  ProcessTimeline,
-  LiveByteInspector,
-  EncodingCompare,
-  FrequencyWaves,
-  NormalizedStepSize,
-  BinaryVsSmooth,
-  RotationVisualization,
+    SplitLayout,
+    Step,
+    Description,
+    Action,
+    Command,
+    ProjectRoadmap,
+    ThinkingProcess,
+    EmbeddingSpace,
+    Callout,
+    ByteStream,
+    table: Table,
+    thead: TableHead,
+    tbody: TableBody,
+    tr: TableRow,
+    th: TableHeader,
+    td: TableCell,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableHeader,
+    TableCell,
+    ComingSoon,
+    ProcessTimeline,
+    LiveByteInspector,
+    EncodingCompare,
+    FrequencyWaves,
+    NormalizedStepSize,
+    BinaryVsSmooth,
+    RotationVisualization,
 };
 
 interface LessonGuideContentProps {
-  source: string;
+    source: string;
 }
 
 export function LessonGuideContent({ source }: LessonGuideContentProps) {
-  return (
-    <MDXRemote
-      source={source}
-      components={components}
-      options={{
-        mdxOptions: {
-          remarkPlugins: [remarkGfm, remarkMath],
-          rehypePlugins: [
-            rehypeKatex,
-            [
-              rehypePrettyCode,
-              {
-                theme: "github-dark",
-                keepBackground: true,
-              },
-            ],
-          ],
-        },
-      }}
-    />
-  );
+    return (
+        <MDXRemote
+            source={source}
+            components={components}
+            options={{
+                blockJS: false,
+                blockDangerousJS: true,
+                mdxOptions: {
+                    remarkPlugins: [remarkGfm, remarkMath],
+                    rehypePlugins: [
+                        rehypeKatex,
+                        [
+                            rehypePrettyCode,
+                            {
+                                theme: "github-dark",
+                                keepBackground: true,
+                            },
+                        ],
+                    ],
+                },
+            }}
+        />
+    );
 }
